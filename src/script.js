bf38555b30e9223be8f43d2e6b8544910501276a
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 11:19:30 +0530

    Compound Assignment With Augmented Division

--- src/script.js
+++ src/script.js
@@ -106,7 +106,7 @@ let c = 3;
 // Only change code below this line
 a -=  6;
 b -=  15;
-c -=  1;\
+c -=  1;
 
 let a = 5;
 let b = 12;
@@ -116,3 +116,12 @@ let c = 4.6;
 a *=  5;
 b *= 3 ;
 c *=  10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=  12;
+b /=  4;
+c /=  11;

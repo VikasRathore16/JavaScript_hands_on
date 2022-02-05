80a45b54c0eff8c7b6be63e92a9204a748d1e3ef
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 14:13:29 +0530

    Accessing Nested Objects

--- src/script.js
+++ src/script.js
@@ -889,3 +889,18 @@ function phoneticLookup(val) {
       "gold": true
     }
   ];
+
+
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"];

8cb1dfde12abea3f0133efb222361aa0f3f1825c
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 13:13:28 +0530

    Golf Code

--- src/script.js
+++ src/script.js
@@ -588,3 +588,19 @@ function testStrictNotEqual(val) {
   }
   
   testSize(7);
+
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }

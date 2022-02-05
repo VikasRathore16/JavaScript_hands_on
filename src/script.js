f70c7d045b1637412dabacd01908a572e5252479
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 13:10:04 +0530

    Chaining If Else Statements

--- src/script.js
+++ src/script.js
@@ -566,4 +566,25 @@ function testStrictNotEqual(val) {
       return "Greater than or equal to 10";
     }
   }
-  orderMyLogic(10)
+  orderMyLogic(10);
+
+
+  function testSize(num) {
+    // Only change code below this line
+   if (num < 5) {
+      return "Tiny";
+    } else if (num < 10) {
+      return "Small";
+    } else if (num < 15) {
+      return "Medium";
+    } else if (num < 20) {
+      return "Large";
+    } else {
+      return "Huge";
+    }
+  
+    return "Change Me";
+    // Only change code above this line
+  }
+  
+  testSize(7);

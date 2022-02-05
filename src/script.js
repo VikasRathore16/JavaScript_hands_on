3c06562d5bb5f0ead11f2fc9a62d8da72024408e
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 11:17:28 +0530

    Compound Assignment With Augmented Multiplication

--- src/script.js
+++ src/script.js
@@ -106,4 +106,13 @@ let c = 3;
 // Only change code below this line
 a -=  6;
 b -=  15;
-c -=  1;
+c -=  1;\
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *=  5;
+b *= 3 ;
+c *=  10;

61b9bf16d548d7e63af5bef363ca35708b131782
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 14:09:41 +0530

    Manipulating Complex Objects

--- src/script.js
+++ src/script.js
@@ -875,3 +875,17 @@ function phoneticLookup(val) {
    
     // Only change code above this line
   }
+
+  const myMusic = [
+    ,{
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];

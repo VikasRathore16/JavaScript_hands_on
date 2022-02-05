8907afa1bb91c72588e377c3bf3b0f687625e8cf
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 13:19:46 +0530

    Adding a Default Option in Switch Statements

--- src/script.js
+++ src/script.js
@@ -624,6 +624,31 @@ function testStrictNotEqual(val) {
         break;
     }
   
+    function switchOfStuff(val) {
+        let answer = "";
+        var a,b,c;
+        // Only change code below this line
+         switch (val) {
+            case "a":
+              answer = "apple";
+              break;
+            case "b":
+              answer = "bird";
+              break;
+            case "c":
+              answer = "cat";
+              break;
+           default:
+              answer = "stuff";
+              break;
+          }
+      
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+      switchOfStuff(1);
   
     // Only change code above this line
     return answer;

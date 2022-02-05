a9a9a386012a55ec3c9f505ff652a3ee1f2b4e61
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-05 10:49:45 +0530

    Understanding Uninitialized Variables

--- src/script.js
+++ src/script.js
@@ -26,3 +26,16 @@ var a=9;
 
 var myFirstName = "Vikas";
 var myLastName ="Rathore";
+
+
+// Only change code below this line
+var a;
+a=5
+var b;
+b=10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
